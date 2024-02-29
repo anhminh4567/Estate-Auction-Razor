27d4aaec7afb701604480d5135deef8f3b465c59
SuongCao <caokhasuong2003@gmail.com>
2024-02-29 21:51:25 +0700

    css homepage(index) + create estate

---
+++ RazorAucionWebapp/MyAttributes/IsCheckBoxNotNull.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorAucionWebapp.MyAttributes
+{
+    public class IsCheckBoxNotNull : ValidationAttribute
+    {
+        public IsCheckBoxNotNull() {
+            ErrorMessage = "At least one estate category must be selected!";
+
+        }
+
+        public override bool IsValid(object? value)
+        {
+            return false;
+        }
+    }
+}
